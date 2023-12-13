41e9ab0160c3685e4904465cf36a12ab10129057
damszew <damian.szewczyk111@gmail.com>
2023-12-13 06:54:51 +0100

    build: Add nix flake dev shell

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        # Only compile time deps
+        nativeBuildInputs = with pkgs; [
+          erlang
+          gleam
+          rebar3
+          commitizen
+        ];
+
+        # Compile & runtime deps
+        buildInputs = with pkgs; [
+        ];
+
+      in
+      with pkgs;
+      {
+        # `nix develop`
+        devShells.default = mkShell {
+          inherit buildInputs nativeBuildInputs;
+        };
+      }
+    );
+}
