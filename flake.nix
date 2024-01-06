7c11f858be56cdd35eb4a0b460600a091505614e
damszew <damian.szewczyk111@gmail.com>
2024-01-06 08:03:47 +0100

    build: Introduce rust to project

--- flake.nix
+++ flake.nix
@@ -4,18 +4,37 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, crane, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ ];
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
 
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override
+          {
+            extensions = [ "rust-analyzer" "rust-src" ];
+          };
+
         # Only compile time deps
         nativeBuildInputs = with pkgs; [
+          rustToolchain
           erlang
           gleam
           rebar3
