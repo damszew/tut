f283f135c23a16773a1102474b1ebccdbeb0d640
damszew <damian.szewczyk111@gmail.com>
2024-01-07 08:11:31 +0100

    refactor: Rust WebSocket sfaccolding

--- src/web/controllers/daily.rs
+++ src/web/controllers/daily.rs
@@ -1,12 +1,23 @@
 use std::collections::HashMap;
 
 use axum::{
-    extract::Path,
+    extract::{
+        ws::{Message, WebSocket},
+        Path, WebSocketUpgrade,
+    },
     response::{IntoResponse, Redirect, Response},
     Form,
 };
+use futures::{
+    sink::SinkExt,
+    stream::{self, StreamExt},
+    Stream,
+};
 
-use crate::web::views::pages::{daily, new_daily};
+use crate::web::views::{
+    components::daily_event,
+    pages::{daily, new_daily},
+};
 
 pub async fn create_form() -> maud::Markup {
     new_daily::page()
@@ -27,3 +38,116 @@ pub async fn room(Path(daily_id): Path<String>) -> Response {
         false => Redirect::to("/").into_response(),
     }
 }
+
+pub async fn websocket(ws: WebSocketUpgrade, Path(daily_id): Path<String>) -> Response {
+    ws.on_upgrade(move |socket| handle_socket(socket, stream::pending()))
+}
+
+async fn handle_socket(
+    socket: WebSocket,
+    mut daily_events: impl Stream<Item = crate::daily::Event> + Unpin + Send + 'static,
+) {
+    tracing::info!("Opened ws");
+    let (mut sender, mut receiver) = socket.split();
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = receiver.next().await {
+            match msg {
+                Message::Close(_) => break,
+
+                Message::Text(_) => {
+                    todo!("Receive msg from WS and pass it to daily");
+                }
+                _ => (),
+            }
+        }
+    });
+
+    let mut send_task = tokio::spawn(async move {
+        while let Some(event) = daily_events.next().await {
+            let text = daily_event::to_html(event).into_string();
+
+            sender
+                .send(Message::Text(text))
+                .await
+                .expect("Handle sender fail case");
+        }
+    });
+
+    tokio::select! {
+        _ = (&mut send_task) => {
+            recv_task.abort();
+        },
+        _ = (&mut recv_task) => {
+            send_task.abort();
+        }
+    }
+}
+
+// pub type WsMessage {
+//   Event(daily.Event)
+// }
+
+// fn handle_ws_message(
+//   daily: daily.Daily,
+//   conn: WebsocketConnection,
+//   message: WebsocketMessage(WsMessage),
+// ) {
+//   case message {
+//     mist.Text(bits) -> {
+//       let msg =
+//         bits
+//         |> bit_array.to_string()
+//         |> result.unwrap("")
+//         |> from_json()
+//         |> to_event()
+
+//       daily
+//       |> daily.send(msg)
+
+//       actor.continue(daily)
+//     }
+
+//     mist.Custom(Event(event)) -> {
+//       let text =
+//         event
+//         |> daily_event.to_html()
+//         |> string_builder.to_string()
+
+//       let assert Ok(_) = mist.send_text_frame(conn, <<text:utf8>>)
+//       actor.continue(daily)
+//     }
+
+//     mist.Binary(_) -> {
+//       actor.continue(daily)
+//     }
+
+//     mist.Closed | mist.Shutdown -> actor.Stop(process.Normal)
+//   }
+// }
+
+// type DailyEventDto {
+//   DailyEventDto(event: String, headers: dynamic.Dynamic)
+// }
+
+// fn from_json(json_string: String) -> DailyEventDto {
+//   let raw_decoder =
+//     dynamic.decode2(
+//       DailyEventDto,
+//       dynamic.field("event", of: dynamic.string),
+//       dynamic.field("HEADERS", of: dynamic.dynamic),
+//     )
+
+//   let assert Ok(result) = json.decode(from: json_string, using: raw_decoder)
+
+//   result
+// }
+
+// fn to_event(msg: DailyEventDto) -> daily.Event {
+//   case msg.event {
+//     "NewPersonJoined" -> daily.NewPersonJoined
+//     "RaisedHand" -> daily.RaisedHand
+//     "PersonLeft" -> daily.PersonLeft
+//     _ -> panic
+//   }
+// }
--- src/web/router.rs
+++ src/web/router.rs
@@ -1,18 +1,15 @@
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use axum::{routing, Router};
 use tower::ServiceBuilder;
 
-use super::controllers;
+use super::controllers::{daily, home};
 
 pub fn router() -> Router {
     Router::new()
-        .route("/", get(controllers::home::page))
-        .route("/daily/new", get(controllers::daily::create_form))
-        .route("/daily", post(controllers::daily::create))
-        .route("/daily/:id", get(controllers::daily::room))
-        // TODO: websocket
+        .route("/", routing::get(home::page))
+        .route("/daily/new", routing::get(daily::create_form))
+        .route("/daily", routing::post(daily::create))
+        .route("/daily/:id", routing::get(daily::room))
+        .route("/daily/:id/ws", routing::get(daily::websocket))
         .layer(ServiceBuilder::new().layer(tower_http::trace::TraceLayer::new_for_http()))
         .with_state(())
 }
